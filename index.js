43f25769fc23f420b8dd406730b2bb3a4a77d330
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-04-28 19:28:38 -0400

    class day one

---
+++ index.js
@@ -0,0 +1,11 @@
+// npm init -y
+ // rm  -rf 
+
+//  const snakeNames = require('snake-names')
+// //  console.log(snakeNames.all)
+// // snakeNames.all.forEach(name => console.log(name))
+// const randomName = snakeNames.random()
+// console.log(randomName)
+
+const http = require('http')
+//npx nodemon 
